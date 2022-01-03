ed7a2327e83ffbb03a51b2f57b2ff8f9e99c0478
Andrey Sevostyanov <andrzejsew@gmail.com>
2022-01-04 00:54:41 +0100

    initialMessage

--- FirstContract.sol
+++ FirstContract.sol
@@ -1,9 +1,18 @@
 pragma solidity ^0.8.7;
 
 contract FirstContract {
-    string public message;
+    
+    string private message;
+
+    constructor(string memory initialMessage) public{
+        message = initialMessage;
+    }
 
     function setMsg(string memory newMessage) public{
         message = newMessage;
+    }
+
+    function getMsg() public view returns (string memory){
+        return message;
     }
 }
