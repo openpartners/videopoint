2e0ba2727e2ab5e1058545e10fb204c4f887d608
Andrey Sevostyanov <andrzejsew@gmail.com>
2022-01-04 11:31:00 +0100

    losowanie

--- Lottery.sol
+++ Lottery.sol
@@ -16,11 +16,29 @@ contract Lottery {
 
     function join() payable public {
         require(msg.value >= .1 ether);
+
         players.push(msg.sender);
         playersCounter++;
 
         if (playersCounter >= maxPlayersNumber) {
             //losowanie
+            drawWinner();
         }
+    }
+
+    function drawWinner() private {
+        uint index = randome() % players.lenght;
+        uint amount = address(this).balance;
+
+        address payable winner = players[index];
+        winner.transfer(amount);
+
+        players = new address payable[](0);
+        playersCounter = 0;
+    }
+
+    function random() private view returns (uint) {
+        return uint(keccak256(
+            abi.encodePacked(block.difficulty, block.timestamp, players)));
     }
 }
